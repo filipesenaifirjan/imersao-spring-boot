530594ff8e673b65d19d4fb6b4556e5860c20533
filipesenaifirjan <filipemattossenai@gmail.com>
2023-01-24 16:33:07 -0300

    initial commit

---
+++ src/main/java/dio/springboot/ImersaoApplication.java
@@ -0,0 +1,16 @@
+package dio.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImersaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImersaoApplication.class, args);
+		
+		
+		
+	}
+
+}
